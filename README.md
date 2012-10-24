c8431777ed74cb9e0bdfe587d70ccda667e875db
Malcolm Christie <malcolm@swanify.com>
2012-10-24 23:00:53 +0100

    Code

--- README.md
+++ README.md
@@ -1,4 +1,48 @@
 Captchas-for-Laravel
 ====================
 
-Super simple captcha model, generates and validates captchas
+Super simple captcha model, generates, validates and clears captchas.
+
+Installation
+============
+
+1) Move captcha.php into your models folder.
+2) Put this code in your routes.php
+```PHP
+Route::get('captcha', function() {
+	return Captcha::make();
+});
+```
+3) Create the captcha table with this
+```MySQL
+CREATE TABLE `<database_name>`.`captchas` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`code` varchar(20) NOT NULL,
+	`updated_at` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP DEFAULT '0000-00-00 00:00:00',
+	`created_at` timestamp NULL DEFAULT '0000-00-00 00:00:00',
+	PRIMARY KEY (`id`)
+) ENGINE=`InnoDB` AUTO_INCREMENT=51 DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ROW_FORMAT=COMPACT CHECKSUM=0 DELAY_KEY_WRITE=0;
+```
+
+Generating a captcha
+--------------------
+```HTML
+<img src="/captcha">
+<input type="text" name="captcha">
+```
+
+Validating a captcha
+--------------------
+```PHP
+if (Captcha::check(Input::get('captcha'))) {
+	// Correct!
+} else {
+	// Wrong :(
+}
+```
+
+Clearing old captchas
+---------------------
+```PHP
+Captcha::clear( $days_old = 1)
+```
---
+++ captcha.php
@@ -0,0 +1,75 @@
+<?
+
+/*
+*	Laravel support added by Malcolm Christie 2012
+*	malcolmchristie.co.uk
+*
+*	Much of this shamelessly taken from Simon Jarvis's code released under the GNU license
+*	http://www.white-hat-web-design.co.uk/articles/php-captcha.php
+*
+*/
+
+class Captcha extends Eloquent {
+
+	public static $table = 'captchas';
+
+	public static function generateCode($characters) {
+		/* list all possible characters, similar looking characters and vowels have been removed */
+		$possible = '23456789bcdfghjkmnpqrstvwxyz';
+		$code = '';
+		$i = 0;
+		while ($i < $characters) { 
+			$code .= substr($possible, mt_rand(0, strlen($possible)-1), 1);
+			$i++;
+		}
+		return $code;
+	}
+
+	public static function make($width='120',$height='40',$characters='6') {
+		$code = self::generateCode($characters);
+		$font = path('storage').'monofont.ttf';
+
+		$save = new Captcha();
+		$save->code = $code;
+		$save->save();
+
+		/* font size will be 75% of the image height */
+		$font_size = $height * 0.75;
+		$image = @imagecreate($width, $height) or die('Cannot initialize new GD image stream');
+
+		/* set the colours */
+		$background_color = imagecolorallocate($image, 244,244,244);
+		$text_color = imagecolorallocate($image, 84, 10, 10);
+		$noise_color = imagecolorallocate($image, 189, 56, 56);
+
+		/* generate random dots in background */
+		for( $i=0; $i<($width*$height)/3; $i++ ) {
+			imagefilledellipse($image, mt_rand(0,$width), mt_rand(0,$height), 1, 1, $noise_color);
+		}
+
+		/* generate random lines in background */
+		for( $i=0; $i<($width*$height)/150; $i++ ) {
+			imageline($image, mt_rand(0,$width), mt_rand(0,$height), mt_rand(0,$width), mt_rand(0,$height), $noise_color);
+		}
+
+		/* create textbox and add text */
+		$textbox = imagettfbbox($font_size, 0, $font, $code) or die('Error in imagettfbbox function');
+		$x = ($width - $textbox[4])/2;
+		$y = ($height - $textbox[5])/2;
+		imagettftext($image, $font_size, 0, $x, $y, $text_color, $font , $code) or die('Error in imagettftext function');
+
+		/* output captcha image to browser */
+		header('Content-Type: image/jpeg');
+		imagejpeg($image);
+		imagedestroy($image);
+	}
+
+	public static function check($code) {
+		$captcha = Captcha::where('code', '=', $code)->first();
+		return ($captcha && $captcha->exists) ? true : false;
+	}
+
+	public static function clear($days = 1) {
+		Captcha::where('created_at', '<', date('c', strtotime("- $days days")))->delete();
+	}
+}
